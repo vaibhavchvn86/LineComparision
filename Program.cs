dd27e902e355ef1ca5850a1a43b1bbe943f14942
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-13 16:23:08 +0530

    solution file

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LineComparision
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("****Welcome to LineComparison Computation Program****");
+
+            #region UC1
+            UC1_linecomparision.Length();
+            #endregion
+
+            #region UC2
+            UC2_LineComparision.Equality();
+            #endregion
+
+            #region UC3
+            UC3_LineComparision.Compare();
+            #endregion
+
+        }
+
+    }
+    }
